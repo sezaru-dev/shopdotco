e22f0501e79a68806b284275e9f8105ff248a497
sezaru-dev <cezarbaguhin20@gmail.com>
2025-02-16 17:28:45 +0800

    Fix sale-banner spacing

--- src/components/PromoBanner.tsx
+++ src/components/PromoBanner.tsx
@@ -11,8 +11,8 @@ const PromoBanner = () => {
   }
   return (
     <div className={`${close && 'hidden'} w-full bg-black text-white py-[9px]`}>
-      <div className='wrapper flex justify-end relative px-4'>
-        <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center font-light text-xs lg:text-sm">Sign up and get 20% off to your first order. 
+      <div className='wrapper flex items-center justify-end relative px-4'>
+        <p className="relative lg:absolute lg:top-1/2 left-1/2 -translate-x-1/2 lg:-translate-y-1/2 text-center font-light text-xs lg:text-sm w-full">Sign up and get 20% off to your first order. 
           <Link href="/" className="ml-1 underline underline-offset-3">Sign Up Now</Link>
         </p>
         <button
